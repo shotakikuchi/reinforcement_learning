cb547c59955556320fdb0b7b85fa4e97838f57a0
grid <s-kikuchi@gridsolar.jp>
2017-10-13 14:16:01 +0900

    n arms bandit （完成）

--- n_arms_bandit/bandit.py
+++ n_arms_bandit/bandit.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 from normal_dist_random import NormalDistRandom
 
 
@@ -12,10 +13,25 @@ class Bandit:
 
         for i in range(size):
             reward_exp = random.get_random()
-            print(reward_exp)
             self.reward_exp[i] = reward_exp
             self.rand_generator[i] = NormalDistRandom(reward_exp, reward_var)
 
-    def select(i):
+    def select(self, i):
         return self.rand_generator[i].get_random()
 
+
+if __name__ == "__main__":
+    bandit = Bandit()
+
+    print("input 0 - %d, or 'q'" % (bandit.size))
+    for line in sys.stdin:
+        line = line.rstrip('\r\n')
+        if line == "q":
+            break
+        elif int(line) in range(10):
+            print(bandit.select(int(line)))
+        else:
+            print("input 0 - %d, or 'q'" % (bandit.size))
+
+    print("expected values")
+    print(bandit.reward_exp)
